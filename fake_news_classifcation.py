63f4a4b7af415fc416a87e9f4faedca4ca946671
Sadrach Pierre <34250199+spierre91@users.noreply.github.com>
2019-11-19 17:26:56 -0500

    Add files via upload

---
+++ fake_news_classifcation.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 14:17:24 2019
+
+@author: sadrachpierre
+"""
+
+import pandas as pd 
+import numpy as np 
+import torch.nn as nn
+from pytorch_pretrained_bert import BertTokenizer, BertModel
+import torch
+from torchnlp.datasets import imdb_dataset
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import classification_report
+
+
+pd.set_option('display.max_columns', None)
+train_data, test_data = imdb_dataset(train=True, test=True)
+df = pd.read_csv("fake.csv")
+df = df[['text', 'type']]
+print(len(df))
+print(df.head())
+
+from collections import Counter 
+
+print(Counter(df['type'].values))
+
+
+df = df[df['type'].isin(['fake', 'satire'])]
+df.dropna(inplace = True)
+df_fake = df[df['type'] == 'fake'] 
+df_statire = df[df['type'] == 'satire'] 
+df_statire = df_statire.sample(n=len(df_fake))
+df = df_statire.append(df_fake)
+df = df.sample(frac=1, random_state = 24).reset_index(drop=True)
+
+print(Counter(df['type'].values))
+
+train_data = df.head(9)
+test_data = df.tail(9)
+
+print(train_data)
+train_data = [{'text': text, 'type': type_data } for text in list(train_data['text']) for type_data in list(train_data['type'])]
+test_data = [{'text': text, 'type': type_data } for text in list(test_data['text']) for type_data in list(test_data['type'])]
+
+train_texts, train_labels = list(zip(*map(lambda d: (d['text'], d['type']), train_data)))
+test_texts, test_labels = list(zip(*map(lambda d: (d['text'], d['type']), test_data)))
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+train_tokens = list(map(lambda t: ['[CLS]'] + tokenizer.tokenize(t)[:511], train_texts))
+test_tokens = list(map(lambda t: ['[CLS]'] + tokenizer.tokenize(t)[:511], test_texts))
+
+
+train_tokens_ids = list(map(tokenizer.convert_tokens_to_ids, train_tokens))
+test_tokens_ids = list(map(tokenizer.convert_tokens_to_ids, test_tokens))
+
+train_tokens_ids = pad_sequences(train_tokens_ids, maxlen=512, truncating="post", padding="post", dtype="int")
+test_tokens_ids = pad_sequences(test_tokens_ids, maxlen=512, truncating="post", padding="post", dtype="int")
+
+train_y = np.array(train_labels) == 'fake'
+test_y = np.array(test_labels) == 'fake'
+
+#
+#
+class BertBinaryClassifier(nn.Module):
+    def __init__(self, dropout=0.1):
+        super(BertBinaryClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 1)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, tokens, masks=None):
+        _, pooled_output = self.bert(tokens, attention_mask=masks, output_all_encoded_layers=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        proba = self.sigmoid(linear_output)
+        return proba
+
+BATCH_SIZE = 1
+EPOCHS = 10
+
+
+train_masks = [[float(i > 0) for i in ii] for ii in train_tokens_ids]
+test_masks = [[float(i > 0) for i in ii] for ii in test_tokens_ids]
+train_masks_tensor = torch.tensor(train_masks)
+test_masks_tensor = torch.tensor(test_masks)
+
+train_tokens_tensor = torch.tensor(train_tokens_ids)
+train_y_tensor = torch.tensor(train_y.reshape(-1, 1)).float()
+test_tokens_tensor = torch.tensor(test_tokens_ids)
+test_y_tensor = torch.tensor(test_y.reshape(-1, 1)).float()
+train_dataset =  torch.utils.data.TensorDataset(train_tokens_tensor, train_masks_tensor, train_y_tensor)
+train_sampler =  torch.utils.data.RandomSampler(train_dataset)
+train_dataloader =  torch.utils.data.DataLoader(train_dataset, sampler=train_sampler, batch_size=BATCH_SIZE)
+test_dataset =  torch.utils.data.TensorDataset(test_tokens_tensor, test_masks_tensor, test_y_tensor)
+test_sampler =  torch.utils.data.SequentialSampler(test_dataset)
+test_dataloader =  torch.utils.data.DataLoader(test_dataset, sampler=test_sampler, batch_size=BATCH_SIZE)
+#
+#
+bert_clf = BertBinaryClassifier()
+optimizer = torch.optim.Adam(bert_clf.parameters(), lr=3e-6)
+
+for epoch_num in range(EPOCHS):
+    bert_clf.train()
+    train_loss = 0
+    for step_num, batch_data in enumerate(train_dataloader):
+        token_ids, masks, labels = tuple(t for t in batch_data)
+        probas = bert_clf(token_ids, masks)
+        loss_func = nn.BCELoss()
+        batch_loss = loss_func(probas, labels)
+        train_loss += batch_loss.item()
+        bert_clf.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+        print('Epoch: ', epoch_num + 1)
+        print("\r" + "{0}/{1} loss: {2} ".format(step_num, len(train_data) / BATCH_SIZE, train_loss / (step_num + 1)))
+
+bert_clf.eval()
+bert_predicted = []
+all_logits = []
+with torch.no_grad():
+    for step_num, batch_data in enumerate(test_dataloader):
+
+        token_ids, masks, labels = tuple(t for t in batch_data)
+
+        logits = bert_clf(token_ids, masks)
+        loss_func = nn.BCELoss()
+        loss = loss_func(logits, labels)
+        numpy_logits = logits.cpu().detach().numpy()
+        
+        bert_predicted += list(numpy_logits[:, 0] > 0.5)
+        all_logits += list(numpy_logits[:, 0])
+        
+print(classification_report(test_y, bert_predicted))
