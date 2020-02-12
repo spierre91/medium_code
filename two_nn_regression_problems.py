7805e2afb6f4a55397c0121aa8203daec9f2934a
Sadrach Pierre <34250199+spierre91@users.noreply.github.com>
2020-02-12 17:19:38 -0500

    Create two_nn_regression_problems.py

---
+++ two_nn_regression_problems.py
@@ -0,0 +1,87 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split 
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv("cars.csv")
+print(df.head())
+X = np.array(df[['age', 'gender', 'miles', 'debt', 'income']])
+y = np.array(df['sales'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
+
+y_train=np.reshape(y_train, (-1,1))
+
+X_scaled = MinMaxScaler()
+y_scaled = MinMaxScaler()
+
+X_scaled.fit(X_train)
+X_scaled = X_scaled.transform(X_train)
+
+y_scaled.fit(X_train)
+y_scaled = y_scaled.transform(y_train)
+
+
+
+
+
+#print(df.head())
+#
+#model = Sequential()
+#model.add(Dense(64, input_dim=5, kernel_initializer='normal', activation='relu'))
+#model.add(Dense(64, activation='relu'))
+#model.add(Dense(1, activation='linear'))
+#model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'], validation_split = 0.2)
+#model.fit(xscale, yscale, epochs=150, batch_size=50, )
+
+
+
+#column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
+#                'Acceleration', 'Model Year', 'Origin']
+#
+##df = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+##df = pd.read_csv(df, names=column_names,
+##                      na_values = "?", comment='\t',
+##                      sep=" ", skipinitialspace=True)
+#df = pd.read_csv("auto-mpg.csv")
+#df.dropna(inplace = True)
+#
+#print(df.head())
+#X = np.array(df[['Cylinders','Displacement','Horsepower','Weight',
+#                'Acceleration', 'Model Year', 'Origin']])
+#y = np.array(df['MPG'])
+#
+#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
+#
+#y_train=np.reshape(y_train, (-1,1))
+#scaler_x = MinMaxScaler()
+##scaler_y = MinMaxScaler()
+#scaler_x_t = MinMaxScaler()
+#
+##print(scaler_y.fit(y_train))
+#print(scaler_x.fit(X_train))
+#print(scaler_x_t.fit(X_test))
+#xscale=scaler_x.transform(X_train)
+#xtscale=scaler_x_t.transform(X_test)
+##yscale=scaler_y.transform(y_train)
+#
+#
+#
+#model = Sequential()
+#model.add(Dense(64, input_dim=7, kernel_initializer='normal', activation='relu'))
+#model.add(Dense(64, activation='relu'))
+#model.add(Dense(1, activation='linear'))
+#optimizer = keras.optimizers.RMSprop(0.001)
+#model.compile(loss='mse', optimizer=optimizer, metrics=['mse','mae'], validation_split = 0.2)
+#model.fit(xscale, y_train, epochs=1000, batch_size=50, )
+#y_pred = model.predict(xtscale)
+#
+#import matplotlib.pyplot as plt 
+#plt.scatter(y_test, y_pred)
+#plt.xlabel('True Values [MPG]')
+#plt.ylabel('Predictions [MPG]')
