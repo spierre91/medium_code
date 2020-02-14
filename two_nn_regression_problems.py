0ff0a3fb7f3c4500b731125c4e5b28a469e12f52
Sadrach Pierre <34250199+spierre91@users.noreply.github.com>
2020-02-14 10:06:00 -0500

    Update two_nn_regression_problems.py

--- two_nn_regression_problems.py
+++ two_nn_regression_problems.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Feb 12 12:48:54 2020
 
@@ -9,12 +7,9 @@ Created on Wed Feb 12 12:48:54 2020
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split 
-from tensorflow import keras
-from tensorflow.keras import layers
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.preprocessing import MinMaxScaler
+
 
 df = pd.read_csv("cars.csv")
 print(df.head())
@@ -25,35 +20,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
 
 y_train=np.reshape(y_train, (-1,1))
 
-X_scaled = MinMaxScaler()
-y_scaled = MinMaxScaler()
-X_test_scaled = MinMaxScaler()
+print(df.head())
 
-
-X_scaled.fit(X_train)
-X_scaled = X_scaled.transform(X_train)
-
-#X_test_scaled.fit(X_test)
-#X_test_scaled = X_test_scaled.transform(X_test)
-
-
-y_scaled.fit(y_train)
-y_scaled = y_scaled.transform(y_train)
-
-
-
-
-
-#print(df.head())
-#
 model = Sequential()
-model.add(Dense(64, input_dim=5, kernel_initializer='normal', activation='relu'))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(32, input_dim=5, kernel_initializer='normal', activation='relu'))
+model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='linear'))
 model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'], validation_split = 0.2)
-model.fit(X_scaled, y_scaled, epochs=100, batch_size=10 )
+model.fit(X_train, y_train, epochs=100, batch_size=10 )
 
 y_pred = model.predict(X_test)
+
+
 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
@@ -62,47 +40,31 @@ plt.scatter(y_test, y_pred)
 plt.xlabel('True Values')
 plt.ylabel('Predictions')
 
-#column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
-#                'Acceleration', 'Model Year', 'Origin']
-#
-##df = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
-##df = pd.read_csv(df, names=column_names,
-##                      na_values = "?", comment='\t',
-##                      sep=" ", skipinitialspace=True)
-#df = pd.read_csv("auto-mpg.csv")
-#df.dropna(inplace = True)
-#
-#print(df.head())
-#X = np.array(df[['Cylinders','Displacement','Horsepower','Weight',
-#                'Acceleration', 'Model Year', 'Origin']])
-#y = np.array(df['MPG'])
-#
-#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
-#
-#y_train=np.reshape(y_train, (-1,1))
-#scaler_x = MinMaxScaler()
-##scaler_y = MinMaxScaler()
-#scaler_x_t = MinMaxScaler()
-#
-##print(scaler_y.fit(y_train))
-#print(scaler_x.fit(X_train))
-#print(scaler_x_t.fit(X_test))
-#xscale=scaler_x.transform(X_train)
-#xtscale=scaler_x_t.transform(X_test)
-##yscale=scaler_y.transform(y_train)
-#
-#
-#
-#model = Sequential()
-#model.add(Dense(64, input_dim=7, kernel_initializer='normal', activation='relu'))
-#model.add(Dense(64, activation='relu'))
-#model.add(Dense(1, activation='linear'))
-#optimizer = keras.optimizers.RMSprop(0.001)
-#model.compile(loss='mse', optimizer=optimizer, metrics=['mse','mae'], validation_split = 0.2)
-#model.fit(xscale, y_train, epochs=1000, batch_size=50, )
-#y_pred = model.predict(xtscale)
-#
-#import matplotlib.pyplot as plt 
-#plt.scatter(y_test, y_pred)
-#plt.xlabel('True Values [MPG]')
-#plt.ylabel('Predictions [MPG]')
+
+df = pd.read_csv("auto-mpg.csv")
+df.dropna(inplace = True)
+del df['Unnamed: 0']
+print(df.head())
+X = np.array(df[['Cylinders','Displacement','Horsepower','Weight',
+                'Acceleration', 'Model Year', 'Origin']])
+y = np.array(df['MPG'])
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
+
+y_train=np.reshape(y_train, (-1,1))
+
+
+model = Sequential()
+model.add(Dense(64, input_dim=7, kernel_initializer='normal', activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'], validation_split = 0.2)
+model.fit(X_train, y_train, epochs=1000, batch_size =10)
+y_pred = model.predict(X_test)
+
+import matplotlib.pyplot as plt 
+plt.scatter(y_test, y_pred)
+plt.xlabel('True Values')
+plt.ylabel('Prediction')
