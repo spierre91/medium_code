7f8847433d3f2fa12ab2b21d3d96646dd4caca06
Sadrach Pierre <34250199+spierre91@users.noreply.github.com>
2020-03-25 16:33:43 -0400

    Create fake_job_class.py

---
+++ BERT_fake_job_classification/fake_job_class.py
@@ -0,0 +1,105 @@
+import pandas as pd 
+import numpy as np 
+import torch.nn as nn
+from pytorch_pretrained_bert import BertTokenizer, BertModel
+import torch
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import classification_report
+
+
+pd.set_option('display.max_columns', None)
+df = pd.read_csv("fake_job_postings.csv")
+#print(df.head())
+
+
+df = df[['description', 'fraudulent']]
+print(df.head())
+
+from collections import Counter
+print(Counter(df['fraudulent'].values))
+
+df_fraudulent= df[df['fraudulent'] == 1] 
+df_normal = df[df['fraudulent'] == 0] 
+df_normal = df_normal.sample(n=len(df_fraudulent))
+df = df_normal.append(df_fraudulent)
+df = df.sample(frac=1, random_state = 24).reset_index(drop=True)
+
+print(Counter(df['fraudulent'].values))
+
+
+train_data = df.head(10)
+test_data = df.tail(10)
+
+train_data = [{'description': description, 'fraudulent': fraudulent } for description in list(train_data['description']) for fraudulent in list(train_data['fraudulent'])]
+test_data = [{'description': description, 'fraudulent': fraudulent } for description in list(test_data['description']) for fraudulent in list(test_data['fraudulent'])]
+
+train_texts, train_labels = list(zip(*map(lambda d: (d['description'], d['fraudulent']), train_data)))
+test_texts, test_labels = list(zip(*map(lambda d: (d['description'], d['fraudulent']), test_data)))
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+train_tokens = list(map(lambda t: ['[CLS]'] + tokenizer.tokenize(t)[:511], train_texts))
+test_tokens = list(map(lambda t: ['[CLS]'] + tokenizer.tokenize(t)[:511], test_texts))
+train_tokens_ids = list(map(tokenizer.convert_tokens_to_ids, train_tokens))
+test_tokens_ids = list(map(tokenizer.convert_tokens_to_ids, test_tokens))
+train_tokens_ids = pad_sequences(train_tokens_ids, maxlen=512, truncating="post", padding="post", dtype="int")
+test_tokens_ids = pad_sequences(test_tokens_ids, maxlen=512, truncating="post", padding="post", dtype="int")
+
+train_y = np.array(train_labels) == 1
+test_y = np.array(test_labels) == 1
+
+
+class BertBinaryClassifier(nn.Module):
+    def __init__(self, dropout=0.1):
+        super(BertBinaryClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 1)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, tokens, masks=None):
+        _, pooled_output = self.bert(tokens, attention_mask=masks, output_all_encoded_layers=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        proba = self.sigmoid(linear_output)
+        return proba
+    
+    
+train_masks = [[float(i > 0) for i in ii] for ii in train_tokens_ids]
+test_masks = [[float(i > 0) for i in ii] for ii in test_tokens_ids]
+train_masks_tensor = torch.tensor(train_masks)
+test_masks_tensor = torch.tensor(test_masks)
+
+train_tokens_tensor = torch.tensor(train_tokens_ids)
+train_y_tensor = torch.tensor(train_y.reshape(-1, 1)).float()
+test_tokens_tensor = torch.tensor(test_tokens_ids)
+test_y_tensor = torch.tensor(test_y.reshape(-1, 1)).float()
+
+
+BATCH_SIZE = 1
+train_dataset =  torch.utils.data.TensorDataset(train_tokens_tensor, train_masks_tensor, train_y_tensor)
+train_sampler =  torch.utils.data.RandomSampler(train_dataset)
+train_dataloader =  torch.utils.data.DataLoader(train_dataset, sampler=train_sampler, batch_size=BATCH_SIZE)
+test_dataset =  torch.utils.data.TensorDataset(test_tokens_tensor, test_masks_tensor, test_y_tensor)
+test_sampler =  torch.utils.data.SequentialSampler(test_dataset)
+test_dataloader =  torch.utils.data.DataLoader(test_dataset, sampler=test_sampler, batch_size=BATCH_SIZE)
+
+
+EPOCHS = 1
+bert_clf = BertBinaryClassifier()
+optimizer = torch.optim.Adam(bert_clf.parameters(), lr=3e-6)
+for epoch_num in range(EPOCHS):
+    bert_clf.train()
+    train_loss = 0
+    for step_num, batch_data in enumerate(train_dataloader):
+        token_ids, masks, labels = tuple(t for t in batch_data)
+        probas = bert_clf(token_ids, masks)
+        loss_func = nn.BCELoss()
+        batch_loss = loss_func(probas, labels)
+        train_loss += batch_loss.item()
+        bert_clf.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+        print('Epoch: ', epoch_num + 1)
+        print("\r" + "{0}/{1} loss: {2} ".format(step_num, len(train_data) / BATCH_SIZE, train_loss / (step_num + 1)))
+        
+print(classification_report(test_y, bert_predicted))
