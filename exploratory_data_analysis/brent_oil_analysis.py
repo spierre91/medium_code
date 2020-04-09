7c53cf9d256052eedd086757527dd6dfac410fa1
Sadrach Pierre <34250199+spierre91@users.noreply.github.com>
2020-04-09 11:37:15 -0400

    Create brent_oil_analysis.py

---
+++ exploratory_data_analysis/brent_oil_analysis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 11:51:27 2019
+@author: sadrachpierre
+"""
+import pandas as pd 
+import matplotlib.pyplot as plt
+df = pd.read_csv("BrentOilPRices.csv")
+df['Date'] = pd.to_datetime(df['Date'])
+print(df.tail())
+
+import seaborn as sns
+sns.set()
+plt.title('Brent Oil Prices')
+sns.lineplot(df['Date'], df['Price'])
+
+df['Price'].hist(bins = 100)
